50c357ad18f00306098722fec77625a7aa124d54
Adham Sherif <adhamsherif7261@gmail.com>
2025-09-01 14:24:33 +0300

    Finished the home modifications (awaiting the vision section) Ar Only + Awaiting The new departments Modifications + Solved The Params awaiting problem + solved all the remaining build problems

--- components/pdf/PdfViewerWrapper.tsx
+++
@@ -1,8 +0,0 @@
-// // components/pdf/PdfViewerWrapper.tsx
-// 'use client';
-
-// import dynamic from 'next/dynamic';
-
-// const PdfViewer = dynamic(() => import('./PdfViewer'), { ssr: false });
-
-// export default PdfViewer;
